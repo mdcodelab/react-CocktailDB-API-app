a1afe690ad98cdabbdc17e188863502c958f8037
miha11-hash <d_mihaela@msn.com>
2023-01-11 22:44:49 +0200

    initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <h2>app component</h2>
+    </div>
+  )
+}
+
+export default App
